b621df9fff037fa3824841a5feac3555b93998b5
Balsa <bakimne008@gmail.com>
2024-12-04 14:33:25 +0100

    reorganizacija

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const animacijaElement = document.querySelector(".animacija");
+//sigma
+  setTimeout(() => {
+    animacijaElement.style.borderRight = "none";
+  }, 1700);
+});
