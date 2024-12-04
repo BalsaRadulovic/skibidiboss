f6e4b0850e2d764450bf057fd5d01c8b102a87dd
Balsa <bakimne008@gmail.com>
2024-12-04 13:27:54 +0100

    nigga

---
+++ app.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const animacijaElement = document.querySelector(".animacija");
+
+  setTimeout(() => {
+    animacijaElement.style.borderRight = "none";
+  }, 1700);
+});
+let ime = document.querySelector("#korisnicko_ime");
+let email = document.querySelector("#email");
+let lozinka = document.querySelector("#lozinka");
+let ponovljenaLozinka = document.querySelector("#ponovi_lozinku");
+let registracijaDugme = document.querySelector("#registracija");
+let ime_greska = document.querySelector("#ime_greska");
+let email_greska = document.querySelector("#email_greska");
+let sifra_greska = document.querySelector("#sifra_greska");
+let pon_sifra_greska = document.querySelector("#ponovljena_sifra_greska");
+
+function provjeriFormu() {
+  const usernamePattern = /^(?=.*[a-z]{2,3}$)/;
+  const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  const passPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!ime.value.match(usernamePattern)) {
+    ime_greska.classList.add("greska");
+  }
+  if (!email.value.match(emailPattern)) {
+    email_greska.classList.add("greska");
+  }
+  if (!lozinka.value.match(passPattern)) {
+    sifra_greska.classList.add("greska");
+  }
+  if (ponovljenaLozinka.value !== lozinka.value) {
+    pon_sifra_greska.classList.add("greska");
+  }
+  if (ime.value.match(usernamePattern)) {
+    ime_greska.classList.remove("greska");
+  }
+  if (email.value.match(emailPattern)) {
+    email_greska.classList.remove("greska");
+  }
+  if (lozinka.value.match(passPattern)) {
+    sifra_greska.classList.remove("greska");
+  }
+  if (ponovljenaLozinka.value === lozinka.value) {
+    pon_sifra_greska.classList.remove("greska");
+  }
+  /*if (ime.value.match(usernamePattern) && email.value.match(emailPattern) && lozinka.value.match(passPattern)) {
+        alert('dobro je')
+    } else {
+        alert('nije dobro')
+    }*/
+}
+
+registracijaDugme.addEventListener("click", (e) => {
+  provjeriFormu();
+
+  if (daLiJeFormIspravna()) {
+    registracijaDugme.style.background = "green";
+  } else {
+    registracijaDugme.style.background = "rgb(255, 91, 91)";
+  }
+});
---
+++ signin.js
@@ -0,0 +1,11 @@
+let ime = document.querySelector('#korisnicko_ime')
+let email = document.querySelector('#email')
+let lozinka = document.querySelector('#lozinka')
+let dugmeRegistracija = document.querySelector('#registracija')
+
+
+dugmeRegistracija.addEventListener('click', () => {
+    
+    console.log(ime.value)
+    console.log(lozinka.value)
+})
