e097bbebba6778e5bf5d424ecd5f6980f22faf5f
AleRadule123 <aleksar.2008164@gmail.com>
2024-12-04 13:42:01 +0100

    SILENCIOOOOO

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const animacijaElement = document.querySelector(".animacija");
-
+//sigma
   setTimeout(() => {
     animacijaElement.style.borderRight = "none";
   }, 1700);
