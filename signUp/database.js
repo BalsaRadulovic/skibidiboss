16effca8b423d091d8ebc92b2c3022765b7dba2d
skibidisaurus <vanja_lazarevic@yahoo.com>
2024-12-04 15:26:48 +0100

    sign up zavrsen

---
+++ signUp/database.js
@@ -0,0 +1,97 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+export const firebaseConfig = {                                                                  //
+    apiKey: "AIzaSyCuFUq9L026A1EptIzpIE21Ptl7DsYz5fo",                                    //
+    authDomain: "sigmaboss-226b5.firebaseapp.com",                                        //
+    projectId: "sigmaboss-226b5",                                                         //
+    storageBucket: "sigmaboss-226b5.firebasestorage.app",                                 //
+    messagingSenderId: "921640413660",                                                    //
+    appId: "1:921640413660:web:2b87c9ae0740bf34d7c719",                                   //
+    databaseURL: "https://sigmaboss-226b5-default-rtdb.europe-west1.firebasedatabase.app" //
+};                                                                                        //
+export const app = firebase.initializeApp(firebaseConfig);                                       //
+export const database = firebase.database();                                                     //
+                                                                                          //
+/*ovde ide config za bazu podataka koju cemo koristiti*/////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+//Object.keys(obj).length za broj propertija u objektu
+//LOCAL STORAGE ZA IME I CURRENT CHAT, PROVERITI DA LI JE MOGUC BREACH I AKO JESTE, NAPRAVITI PREVENCIJU
+export async function dobijPodatke(lokacija) {
+    const temp=await database.ref(lokacija).get()
+    .then(snapshot => {
+    if (snapshot.exists()) {
+        return snapshot.val();
+    } else {
+        return {};
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+    return temp;
+}
+export async function noviKorisnik(username,password,email) { 
+    const nalozi=Object.values(await dobijPodatke("korisnici"))
+    const length=Object.keys(nalozi).length
+    let imeZauzeto=false;
+    for (let i=0;i<length;i++) {
+        if (nalozi[i].user==username) {imeZauzeto=true;}
+    }
+    if (!imeZauzeto) {
+        const korisnik = {
+            user:username,
+            password:password,
+            email:email,
+            ID:length+1,
+            chatovi:[]
+        }
+        database.ref('korisnici/'+(length+1)).update(korisnik);
+        return true;
+    } else {return false;}
+    
+}
+export async function noviChat(ucesnici,naziv) {
+    const chatovi=await dobijPodatke("chatovi") //ne znam da li ovo omogucava breach
+    const brChatova=Object.keys(chatovi).length
+        const chat= {
+            naziv:naziv,
+            ID:brChatova+1,
+            ucesnici:ucesnici,
+            brojPoruka:0,
+            poruke:[]
+        }
+    const broj=brChatova+1;
+    database.ref("chatovi/"+broj).update(chat);
+    const korisnici=Object.values(await dobijPodatke("korisnici"))
+    for (let i=0;i<korisnici.length;i++) {
+        for (let j=0;j<ucesnici.length;j++) {
+            if (ucesnici[j]==korisnici[i].user) {
+                const konkretanKorisnik=korisnici[i];
+                database.ref("korisnici/"+konkretanKorisnik).update({[konkretanKorisnik.chatovi.length+1]:ID})
+            }
+        }
+    }
+}
+export async function novaPoruka(sadrzaj,sender,openChat) {
+    const poruka= {
+        sadrzaj:sadrzaj,
+        sender:sender
+    }
+    const chat=await dobijPodatke("chatovi/"+openChat)
+    const brUcesnika=chat.ucesnici.length
+    for (let i=0;i<brUcesnika;i++) {
+        if (sender==chat.ucesnici[i]) {
+            database.ref("chatovi/"+openChat+"/poruke").update({[chat.brojPoruka]:poruka})
+            database.ref("chatovi/"+openChat).update({brojPoruka:(chat.brojPoruka)+1})
+            return;
+        } else {/*error*/ console.log("zao mi je niste u grupi"); return;}
+    }
+    
+}
+export async function proveraLogin(user,pass) {
+    const nalozi=Object.values(await dobijPodatke("korisnici")) //ne znam da li ovo omogucava breach
+    const length=Object.keys(nalozi).length
+    for (let i=0;i<length;i++) {
+        if (nalozi[i].user==user && nalozi[i].password==pass) {return true;}
+    }
+    return false;
+}
--- signUp/signUp.js
+++ signUp/signUp.js
@@ -1,44 +1,47 @@
-let ime = document.querySelector("#korisnicko_ime");
-let email = document.querySelector("#email");
-let lozinka = document.querySelector("#lozinka");
-let ponovljenaLozinka = document.querySelector("#ponovi_lozinku");
+let userField = document.querySelector("#korisnicko_ime");
+let emailField = document.querySelector("#email");
+let lozinkaField = document.querySelector("#lozinka");
+let ponovljenaLozinkaField = document.querySelector("#ponovi_lozinku");
 let registracijaDugme = document.querySelector("#registracija");
 let ime_greska = document.querySelector("#ime_greska");
 let email_greska = document.querySelector("#email_greska");
 let sifra_greska = document.querySelector("#sifra_greska");
 let pon_sifra_greska = document.querySelector("#ponovljena_sifra_greska");
+let imeZauzeto = document.querySelector("#korisnickoImePostoji");
+import {firebaseConfig,app,database,dobijPodatke,noviKorisnik,noviChat,novaPoruka,proveraLogin} from "./database.js"
 
 function provjeriFormu() {
-    const usernamePattern = /^(?=.*[a-z]{2,3}$)/;
+    const usernamePattern = /^[a-z0-9]{3,10}$/;
     const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-    const passPattern =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    if (!ime.value.match(usernamePattern)) {
+    const passPattern =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    let user=userField.value;
+    let email=emailField.value;
+    let password=lozinkaField.value;
+    let ponovljenPassword=ponovljenaLozinkaField.value;
+    let greska=false;
+    if (!user.match(usernamePattern)) {
         ime_greska.classList.add("greska");
-    }
-    if (!email.value.match(emailPattern)) {
+        greska=true;
+    } else {ime_greska.classList.remove("greska");}
+    if (!email.match(emailPattern)) {
         email_greska.classList.add("greska");
-    }
-    if (!lozinka.value.match(passPattern)) {
+        greska=true;
+    } else {email_greska.classList.remove("greska");}
+    if (!password.match(passPattern)) {
         sifra_greska.classList.add("greska");
-    }
-    if (ponovljenaLozinka.value !== lozinka.value) {
+        greska=true;
+    } else {sifra_greska.classList.remove("greska");}
+    if (ponovljenPassword !== password) {
         pon_sifra_greska.classList.add("greska");
-    }
-    if (ime.value.match(usernamePattern)) {
-        ime_greska.classList.remove("greska");
-    }
-    if (email.value.match(emailPattern)) {
-        email_greska.classList.remove("greska");
-    }
-    if (lozinka.value.match(passPattern)) {
-        sifra_greska.classList.remove("greska");
-    }
-    if (ponovljenaLozinka.value === lozinka.value) {
-        pon_sifra_greska.classList.remove("greska");
+        greska=true;
+    } else {pon_sifra_greska.classList.remove("greska");}
+    if (greska) {return;}
+    if (!noviKorisnik(user,password,email)) {
+        imeZauzeto.classList.add("greska")
     }
 }
 
 registracijaDugme.addEventListener("click", (e) => {
     provjeriFormu();
+    console.log("click radi")
 });
