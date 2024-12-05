a4a5a05a4ccdaa8e30baf12e19740102db2d90f1
skibidisaurus <vanja_lazarevic@yahoo.com>
2024-12-05 21:00:05 +0100

    mozda universal chat

--- chat/chat.js
+++ chat/chat.js
@@ -13,37 +13,39 @@ let trenutnaVisina=0;
 let brojUcitanihPoruka=0;
 let openChat=1;//treba local storage
 let sender="sigma"
-let ukupanBrojPoruka;
+let poslednjaPorukaID;
 const intervalPoruka=20;
-
+let ukupanBrojPoruka;
 dugme.addEventListener("click",async () => {
     if (unos.value!="") {
         novaPoruka(unos.value,sender,openChat)
-        await loadNewMessage(unos.value,sender,openChat)
+        reload(0)
         iframe.contentWindow.scrollTo(0,ekranMali.scrollHeight)
     }
 })
-async function loadNewMessage(tekst,sender,openChat) {
-    const poruke=await dobijPodatke("chatovi/"+openChat+"/poruke")//poruke iz tog chata u arrayu
-    let idPoslednjePoruke=Object.keys(poruke).length-1
-    const celaPoruka=document.createElement("div");
-    const textPoruke=document.createElement("div"); textPoruke.textContent=tekst; textPoruke.classList.add("textporuke");
-    const imeSendera=document.createElement("div"); imeSendera.textContent=sender; imeSendera.classList.add("sender");
-    if (sender!=poruke[idPoslednjePoruke-1].sender) {celaPoruka.appendChild(imeSendera)}
-    celaPoruka.appendChild(textPoruke); celaPoruka.classList.add("poruka")
-    ekranMali.appendChild(celaPoruka);
-    ukupanBrojPoruka++;
-    trenutnaVisina+=celaPoruka.clientHeight;
-}
+
+// async function loadNewMessage(tekst,sender,openChat) {
+//     const poruke=await dobijPodatke("chatovi/"+openChat+"/poruke")//poruke iz tog chata u arrayu
+//     let idPoslednjePoruke=Object.keys(poruke).length-1
+//     const celaPoruka=document.createElement("div");
+//     const textPoruke=document.createElement("div"); textPoruke.textContent=poruke[i].sadrzaj; textPoruke.classList.add("textporuke");///////////
+//     const imeSendera=document.createElement("div"); imeSendera.textContent=poruke[i].sender; imeSendera.classList.add("sender");/////////////////
+//     if (sender!=poruke[idPoslednjePoruke-1].sender) {celaPoruka.appendChild(imeSendera)}
+//     celaPoruka.appendChild(textPoruke); celaPoruka.classList.add("poruka")
+//     ekranMali.appendChild(celaPoruka);
+//     ukupanBrojPoruka++;
+//     trenutnaVisina+=celaPoruka.clientHeight;
+// }
 function removeSve() {
     const elementsToDelete = iframeDoc.querySelectorAll('.poruka');
     elementsToDelete.forEach(element => {
         element.remove();
     });
 }
-async function reload() {
+async function reload(uslov) {
     const poruke=await dobijPodatke("chatovi/"+openChat+"/poruke")//poruke iz tog chata u arrayu
     ukupanBrojPoruka=Object.keys(poruke).length
+    poslednjaPorukaID=ukupanBrojPoruka-1;
     let daLiMozeNastaviti=true;
     let start=0;
     for (let i=ukupanBrojPoruka-1;daLiMozeNastaviti;i--) {
@@ -59,12 +61,15 @@ async function reload() {
       celaPoruka.appendChild(textPoruke); celaPoruka.classList.add("poruka")
       ekranMali.appendChild(celaPoruka);
     }
-    iframe.contentWindow.scrollTo(0, ekranMali.scrollHeight-trenutnaVisina)
+    iframe.contentWindow.scrollTo(0, ekranMali.scrollHeight-trenutnaVisina*uslov)
     trenutnaVisina=ekranMali.scrollHeight;
     brojUcitanihPoruka=ukupanBrojPoruka-start;
 }
-function uslovnoGenerisanje() {
-    if (brojUcitanihPoruka<ukupanBrojPoruka && iframeDoc.documentElement.scrollTop==0) {reload()}
+async function uslovnoGenerisanje() {
+    const poruke=await dobijPodatke("chatovi/"+openChat+"/poruke")//poruke iz tog chata u arrayu
+    let novPoslednjiID=Object.keys(poruke).length-1
+    if (poslednjaPorukaID<novPoslednjiID) {reload(0)} else
+    if ((brojUcitanihPoruka<ukupanBrojPoruka && iframeDoc.documentElement.scrollTop==0)) {reload(1)}
 }
-await reload();
-setInterval(uslovnoGenerisanje,5000)
+await reload(1);
+setInterval(uslovnoGenerisanje,1000)
